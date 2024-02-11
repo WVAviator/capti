36d6fc462e86a32b444433678934d7dd2aac67fd
Wvaviator <wvaviator@gmail.com>
2024-02-11 15:56:00 -0600

    update build scripts for linux

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
+OS=$(uname -s)
+ARCH=$(uname -m)
+
+if [ "$OS" != "Linux" ]; then
+  echo "This script is only for Linux"
+  exit 1
+fi
+
+if [ "$ARCH" != "x86_64" ]; then
+  echo "This script is only for x86_64"
+  exit 1
+fi
+
 rustup target add x86_64-unknown-linux-gnu 2> /dev/null
 rustup target add aarch64-unknown-linux-gnu 2> /dev/null
+
+dpkg -l grep -qw build-essential || sudo apt-get install build-essential
+dpkg -l grep -qw gcc-aarch64-linux-gnu || sudo apt-get install gcc-aarch64-linux-gnu
+
+PACKAGES=(gcc-aarch64-linux-gnu g++-aarch64-linux-gnu gcc g++ make cmake build-essential)
+
+for package in "${PACKAGES[@]}"; do
+  dpkg -l grep -qw $package || sudo apt-get install $package
+done
 
 cargo build --release --target x86_64-unknown-linux-gnu
 cargo build --release --target aarch64-unknown-linux-gnu 
