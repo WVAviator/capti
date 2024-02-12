7ae3580dd8b90a4927deaaecf091d9de9b3215ac
Wvaviator <wvaviator@gmail.com>
2024-02-11 19:33:08 -0600

    switch back to esmodules

--- npm/bin/capti.js
+++ npm/bin/capti.js
@@ -4,6 +4,9 @@ import os from "os";
 import path from 'path';
 import { spawn } from "child_process";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const DIST_DIR = path.resolve(__dirname, "..", "dist");
 
 const extension = os.platform() === "win32" ? ".exe" : "";
