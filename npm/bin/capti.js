cf37d7031b16567e0ab6b02b891126de40e6be4f
Wvaviator <wvaviator@gmail.com>
2024-02-11 19:34:08 -0600

    correct dirname imports

--- npm/bin/capti.js
+++ npm/bin/capti.js
@@ -3,6 +3,7 @@
 import os from "os";
 import path from 'path';
 import { spawn } from "child_process";
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
